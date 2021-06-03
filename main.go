84745343ec9ed2c2315ab765627bc318452804d8
brenu <bvsousa64@gmail.com>
2021-06-02 22:59:47 -0300

    :rocket: Add condition in order to update games based on the last one

--- main.go
+++ main.go
@@ -23,6 +23,20 @@ func handleNewGiveaways(lastGiveaway GiveAway, twitterClient *twitter.Client) Gi
 		if err != nil {
 			log.Fatalf(fmt.Sprint("Bad news here, reason: ", err.Error()))
 		}
+	} else if lastGiveaway.ID != newGiveaways[len(newGiveaways)-1].ID {
+		newGiveawaysLength := len(newGiveaways)
+
+		for i := 0; i < newGiveawaysLength && newGiveaways[i].ID != lastGiveaway.ID; i++ {
+			newLastGiveaway := newGiveaways[i]
+
+			tweetString := fmt.Sprintf("%s - %s\n\nAvailable on: %s", newLastGiveaway.Title, newLastGiveaway.Platforms, newLastGiveaway.GamerpowerURL)
+
+			_, _, err := twitterClient.Statuses.Update(tweetString, &twitter.StatusUpdateParams{})
+
+			if err != nil {
+				log.Fatalf(fmt.Sprint("Bad news here, reason: ", err.Error()))
+			}
+		}
 	}
 
 	return newGiveaways[len(newGiveaways)-1]
